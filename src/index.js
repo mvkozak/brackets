a79e469e09e4e7a2c2cf8d73f1bc541072c3c870
mvkozak <maryiakazak@gmail.com>
2020-03-22 17:00:04 +0300

    feat:add solution for brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  bracketsConfig = bracketsConfig.map(item => item.reduce ((acc, cur) => acc + cur));
+ 
+  for (let i = str.length; i >= 0; i--) {
+    bracketsConfig.forEach (item => {
+      str = str.replace(item, '');
+    });
+    if (str.length == 0) return true;
+  } return false;
 }
